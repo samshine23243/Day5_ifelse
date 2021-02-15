fc49b493c5783892fc5c5aceaf4231b1c184b7d8
samshine <sameershinde23243@gmail.com>
2021-02-15 12:27:53 +0530

    example 1  code added

--- ifelse.sh
+++ ifelse.sh
@@ -1,1 +1,21 @@
-echo "if else problems"
+
+#!/bin/bash
+declare -a Array
+min=9999
+max=-9999
+for i in {1..5}
+do
+   Array[i]=$((100+$RANDOM%900))
+done
+for i in "${Array[@]}"
+do
+   echo $i
+   if [ $i -lt $min ]
+   then
+      min=$i
+   fi
+   if [ $i -gt $max ]
+   then
+      max=$i
+   fi
+done
